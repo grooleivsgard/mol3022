e9c000bac377b85ddd2f920d72c6a00ab4bfa1ff
grooleivsgard <gro.oleivsgard@gmail.com>
2024-03-18 15:15:14 +0100

    Add ignore and requirements file

--- motif_analysis.py
+++ motif_analysis.py
@@ -6,4 +6,19 @@ class JASPARMotifAnalyzer:
 
     # Get the motif PFM from JASPAR
     def get_motif_pfm(self, motif):
-        # check if m
+        # check if motif is the motif name or the motif ID
+        if motif.startswith('MA'):
+            return self.jdb_obj.fetch_motif_by_id(motif)
+        else:
+            return self.jdb_obj.fetch_motifs_by_name(motif)
+
+    # Get the motif PWM
+    def get_motif_pwm(self, motif):
+        return self.get_motif_pfm(motif).pwm
+
+if __name__ == "__main__":
+    # Example: get the Probability Frequency Matrix (PFM) for PDX1 (a transcriptional activator of several genes, incl insulin)
+    analyzer = JASPARMotifAnalyzer()
+    motif = analyzer.get_motif_pwm('MA0132.2')
+    print(motif)
+
