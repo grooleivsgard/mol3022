51f4dae0178b58454f2e59e47579d34492b68a20
grooleivsgard <gro.oleivsgard@gmail.com>
2024-03-19 13:53:36 +0100

    update structure

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
-from motif_analyzer import JASPARMotifAnalyzer
+from calculations import scan_sequence, plot_scores
+from data_retrieval import MotifAnalyzer, SequenceParser
+
+# Example: 
+filename = "data/sequence.fasta" # Example fasta file
+threshold = 0.5  # Example threshold
+motif = "MA0132.2"  # Example motif
 
 if __name__ == "__main__":
-    # Example: get the Probability Frequency Matrix (PFM) for PDX1 (a transcriptional activator of several genes, incl insulin)
-    analyzer = JASPARMotifAnalyzer()
-    motif = analyzer.get_motif_pwm('MA0132.2')
-    print(motif)
+    pwm = MotifAnalyzer().get_motif_pwm(motif)
+    dna_sequence = SequenceParser(filename).read_fasta()
+    # Scan the sequence
+    potential_sites, scores = scan_sequence(pwm, dna_sequence, threshold)
+    # Visualize results
+    plot_scores(scores, threshold)
 
