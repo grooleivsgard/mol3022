4314c049ab8b1510952c52d8449808ea9269c3a6
grooleivsgard <gro.oleivsgard@gmail.com>
2024-04-08 16:27:52 +0200

    Add multiple input fields

--- src/main.py
+++ src/main.py
@@ -2,31 +2,45 @@ import tkinter as tk
 from tkinter import ttk
 from event_handlers import handle_submit
 from event_handlers import exit_program
-import sys
 
+# Fields for the form
+fields = ('PWM', 'NCBI ID', 'Email')
+
+# Function to fetch the values from the form
+def fetch(entries):
+        for entry in entries:
+            field = entry[0]
+            text  = entry[1].get()
+            print('%s: "%s"' % (field, text))
+
+# Function to create the form
+def makeform(root, fields):
+    entries = []
+    for field in fields:
+        row = tk.Frame(root)
+        lab = tk.Label(row, width=15, text=field, anchor='w')
+        ent = tk.Entry(row)
+        row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        lab.pack(side=tk.LEFT)
+        ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+        entries.append((field, ent))
+    return entries
+    
 if __name__ == "__main__":
     # Create the main window
     root = tk.Tk()
-    root.title("Identify the most likely transcription factor binding sites in the DNA sequence")
+    ents = makeform(root, fields)
 
-    # Create an input field where the user can enter data
-    input_label = tk.Label(root, text="Enter your Matrix ID:")
-    input_label.pack()
+    # Placeholder for the PWM output label
+    output_pwm = tk.Label(root, text="")
+    output_pwm.pack()
 
-    # Create an input field where the user can enter data
-    entry = tk.Entry(root)
-    entry.pack()
-    # Create a button to submit the data
-    submit_button = tk.Button(root, text="Submit", command=lambda: handle_submit(entry, pwm_output, root))
-    submit_button.pack()
+    # Add a submit button
+    b1 = tk.Button(root, text="Submit", command=lambda: handle_submit(ents, output_pwm, root))
+    b1.pack(side=tk.LEFT, padx=5, pady=5)
+    
+    # Add a quit button
+    b2 = tk.Button(root, text="Quit", command=lambda: exit_program(root))
+    b2.pack(side=tk.LEFT, padx=5, pady=5)
 
-    # Create a label to display the PWM output
-    pwm_output = tk.Label(root, text="")
-    pwm_output.pack()
-
-    # Create a button to exit the program
-    exit_button = tk.Button(root, text="Exit", command=sys.exit)
-    exit_button.pack()
-
-    # Start the main loop
     root.mainloop()
