b14881d7dc5eed5302463cb3f8196dc0adf2f569
grooleivsgard <gro.oleivsgard@gmail.com>
2024-04-08 16:26:48 +0200

    Handle submission of multiple input fields

--- src/event_handlers.py
+++ src/event_handlers.py
@@ -1,33 +1,55 @@
 from calculations import scan_sequence
-from data_retrieval import MotifAnalyzer, SequenceParser
+from data_retrieval import fetch_motif_pwm, fetch_sequence
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import itertools
 
-# Example:
-filename = "data/sequence.fasta"  # Example fasta file
-threshold = 0.5  # Example threshold
 
-def handle_submit(entry, pwm_output, root):
-    # Function to handle what happens when the user clicks the "Submit" button
-    user_input = entry.get()  # Get the text entered by the user
-    pwm = MotifAnalyzer().get_motif_pwm(user_input)
-    pwm_output.config(text=f"{pwm}")
+# NOT FINISHED
+def validate(entries):
+    # Function to validate the input
+    valid = True
 
-    # Read the DNA sequence
-    dna_sequence = SequenceParser(filename).read_fasta()
-    # Scan the sequence
-    probabilities, scores, binding_site = scan_sequence(pwm, dna_sequence, threshold)
-    print(binding_site)
+    for field, entry in entries:
+        text = entry.get().strip()  # Remove leading/trailing whitespaces
+        if not text:  # Basic check for non-empty
+            print(f"Error: {field} is required.")
+            valid = False
+            continue  # Add more specific checks as needed
+        
+        # Example: specific field validation
+        if field == 'Email' and '@' not in text:
+            print(f"Error: {field} doesn't seem to be a valid email.")
+            valid = False
+    return valid
+
+
+def handle_submit(entries, output_pwm, root):
+    
+    # Function to handle the submit button click
+    pwm_id, ncbi_id, email = [entry[1].get().strip() for entry in entries]
+
+    # Fetch the PWM
+    pwm = fetch_motif_pwm(pwm_id)
+    output_pwm.config(text=f"{pwm}")
+
+    # Fetch the DNA sequence
+    dna_sequence = fetch_sequence(ncbi_id, email)
+
+    # Scan the DNA sequence for potential binding sites
+    scores, binding_site = scan_sequence(pwm, dna_sequence)
+
     # Plot scores
     fig = plot_scores(scores, binding_site, root)
     canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
     canvas_widget = canvas.get_tk_widget()
     canvas_widget.pack()
 
+# Function to plot the scores
 def plot_scores(scores, binding_site, root):
-    fig, ax = plt.subplots(figsize=(10, 6))
 
+    fig, ax = plt.subplots(figsize=(10, 6))
     colors = itertools.cycle(['blue', 'green', 'red'])  # Use a cyclic color palette
 
     for i, score in enumerate(scores):
@@ -49,6 +71,5 @@ def plot_scores(scores, binding_site, root):
     return fig
 
 
-def exit_program():
+def exit_program(root):
     root.destroy()  # Avslutter GUI-vinduet og dermed programmet
-    sys.exit()
